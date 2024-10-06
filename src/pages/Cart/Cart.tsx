498ea5a0ffddee665fc369105a4346b1cbe54b89
4nh3k <21520579@gm.uit.edu.vn>
2024-10-06 23:27:10 +0700

    update cart page - add recommendation section

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,4 +1,6 @@
 import { Button, TextInput } from "flowbite-react";
+import { ProductList } from "../../assets/mockdata";
+import Product from "../../components/Product";
 import CartProduct from "../../components/Product/CartProduct";
 
 export function Cart() {
@@ -72,6 +74,19 @@ export function Cart() {
             </Button>
           </div>
         </div>
+      </div>
+      <div className="heading-4 mt-10">People also bought</div>
+      <div className="flex w-full space-x-4 mt-4">
+        {ProductList.slice(0, 5).map((product) => (
+          <Product
+            title={product.title}
+            imageURL={product.imageURL}
+            price={product.price}
+            rating={product.rating}
+            discount={product.discount}
+            totalRating={product.totalRating}
+          />
+        ))}
       </div>
     </>
   );
