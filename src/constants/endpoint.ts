23746b5d9faaca3db9c577baa31cf30f64beb99d
SurgeousJP <tuanbao2311@gmail.com>
2024-10-14 06:50:16 +0700

    Create all api routes in endpoint.ts

--- src/constants/endpoint.ts
+++ src/constants/endpoint.ts
@@ -1,10 +1,29 @@
 export const URL_BASE = "http://localhost:6001";
+// Identity api
 export const IDENTITY_PREFIX = "/identity.api/v1";
-export const CATALOG_PREFIX = "/catalog.api/v1";
-export const CART_PREFIX = "/basket.api/v1";
 export const URL_REGISTER = "/users/register";
 export const URL_LOGIN = "/users/login";
 export const URL_LOGOUT = "/users/logout";
+
+// Catalog api
+export const CATALOG_PREFIX = "/catalog.api/v1";
 export const URL_BOOKS = "/books";
 export const URL_GENRES = "/genres";
+export const URL_BOOK_REVIEWS = "/book-reviews"
+export const URL_FORMATS = "/formats"
+export const URL_PUBLISHERS = "/publishers"
+
+// Basket api
+export const CART_PREFIX = "/basket.api/v1";
 export const URL_CART = "/baskets";
+
+// Ordering api
+export const ORDERING_PREFIX = "/ordering.api/v1"
+export const URL_ORDERS = "/orders"
+export const URL_PAYMENT = "/payment-webhook"
+export const URL_TRANSACTIONS = "/transactions"
+
+// RecSys api
+export const RECSYS_PREFIX = "/recsys.api"
+export const URL_PREDICT = "/predict"
+
