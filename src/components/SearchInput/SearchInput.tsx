5395b7969d787d45725cb99a305a9c3ff526ed9d
4nh3k <21520579@gm.uit.edu.vn>
2024-10-16 02:05:45 +0700

    Refactor SearchInput component

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -4,28 +4,39 @@ import customSelectTheme from "./SelectTheme";
 interface SearchInputProps {
   className: string;
   placeholder: string;
-  dropdownList: string[];
+  dropdownList?: string[];
   dropdownLabel: string;
-  enableDropdown?: boolean
+  enableDropdown?: boolean;
 }
 
-export function SearchInput(props: SearchInputProps) {
+export function SearchInput({
+  className,
+  dropdownLabel,
+  dropdownList,
+  placeholder,
+  enableDropdown = true,
+}: SearchInputProps) {
   return (
-    <form className={props.className}>
+    <form className={className}>
       <div className="flex">
-        {props.dropdownList.length > 0? <Select id="countries" theme={customSelectTheme} required>
-        </Select> : ''}
-        <div className="relative w-full">
+        {enableDropdown && dropdownList!.length > 0 ? (
+          <Select id="countries" theme={customSelectTheme} required></Select>
+        ) : (
+          <></>
+        )}
+        <div className={`relative w-full`}>
           <input
             type="search"
             id="search-dropdown"
-            className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
-            placeholder={props.placeholder}
+            className={`block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg ${
+              !enableDropdown ? "rounded-l-lg" : "border-s-gray-50 border-s-2 "
+            } border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500`}
+            placeholder={placeholder}
             required
           />
           <button
             type="submit"
-            className="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="absolute rounded-r-lg top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             <svg
               className="w-4 h-4"
--- src/pages/AdminPage/BookList/BookGridPage.tsx
+++ src/pages/AdminPage/BookList/BookGridPage.tsx
@@ -1,47 +1,57 @@
-import SearchInput from '../../../components/SearchInput'
-import { Pagination, Select } from 'flowbite-react'
+import { Pagination, Select } from "flowbite-react";
 import { ProductList } from "../../../assets/mockdata";
-import Product from '../../../components/Product';
+import Product from "../../../components/Product";
+import SearchInput from "../../../components/SearchInput";
 const BookGridPage = () => {
-	const dropdownList =
-		['Price (Low to High)', 'Price (High to Low)', 'Avg Reviews'
-		];
-	return (
-		<div className='bg-white flex flex-col mt-5 px-4 py-4 flex-start flex-shrink-0 min-h-screen gap-6 rounded-lg shadow-sm'>
-			<span className='text-[1.5rem] font-bold'>Book</span>
-			<div className='flex justify-between items-center self-stretch'>
-				<SearchInput className={''} placeholder={'Search book'} dropdownList={[]} dropdownLabel={''}></SearchInput>
-				<div className='flex justify-end items-center gap-3'>
-					<span className='text-[1rem] font-normal'>
-						Sort by
-					</span>
-					<Select required >
-						{dropdownList.map((item, index) => (
-							<option key={index} value={item}>
-								{item}
-							</option>
-						))}
-					</Select>
-				</div>
+  const dropdownList = [
+    "Price (Low to High)",
+    "Price (High to Low)",
+    "Avg Reviews",
+  ];
+  return (
+    <div className="bg-white flex flex-col mt-5 px-4 py-4 flex-start flex-shrink-0 min-h-screen gap-6 rounded-lg shadow-sm">
+      <span className="text-[1.5rem] font-bold">Book</span>
+      <div className="flex justify-between items-center self-stretch">
+        <SearchInput
+          className={""}
+          placeholder={"Search book"}
+          dropdownList={[]}
+          dropdownLabel={""}
+          enableDropdown={false}
+        ></SearchInput>
+        <div className="flex justify-end items-center gap-3">
+          <span className="text-[1rem] font-normal">Sort by</span>
+          <Select required>
+            {dropdownList.map((item, index) => (
+              <option key={index} value={item}>
+                {item}
+              </option>
+            ))}
+          </Select>
+        </div>
+      </div>
+      <div className="grid grid-cols-4 justify-items-center w-full gap-[4.5rem] ">
+        {ProductList.map((product) => (
+          <Product
+            title={product.title}
+            imageURL={product.imageURL}
+            price={product.price}
+            rating={product.rating}
+            discount={product.discount}
+            totalRating={product.totalRating}
+          />
+        ))}
+      </div>
+      <Pagination
+        className="m-auto"
+        currentPage={0}
+        onPageChange={function (): void {
+          throw new Error("Function not implemented.");
+        }}
+        totalPages={3}
+      ></Pagination>
+    </div>
+  );
+};
 
-			</div>
-			<div className="grid grid-cols-4 justify-items-center w-full gap-[4.5rem] ">
-				{ProductList.map((product) => (
-					<Product
-						title={product.title}
-						imageURL={product.imageURL}
-						price={product.price}
-						rating={product.rating}
-						discount={product.discount}
-						totalRating={product.totalRating}
-					/>
-				))}
-			</div>
-			<Pagination className='m-auto' currentPage={0} onPageChange={function (): void {
-				throw new Error('Function not implemented.');
-			}} totalPages={3}></Pagination>
-		</div>
-	)
-}
-
-export default BookGridPage
+export default BookGridPage;
