888780270d381b70fd84254239a34f9afae0fea6
SurgeousJP <tuanbao2311@gmail.com>
2024-10-17 22:16:08 +0700

    Add coupon component and coupon page

--- src/components/Coupon/Coupon.tsx
+++ src/components/Coupon/Coupon.tsx
@@ -2,10 +2,15 @@ import React from 'react'
 
 const Coupon = () => {
   return (
-    <div className='w-[400px] h-[164px]'>
-      <div className='flex w-[150px] h-[150px] py-[37px] px-[29px] flex-col items-start gap-2 flex-shrink-0 rounded-l-lg border-1 border-solid border-blue-200 bg-blue-50 relative'>
+    <div className='w-full h-[160px] flex'>
+      <div class='rounded-invert-right'>
         <span className='text-primary text-center text-4xl font-bold leading-9'>$9</span>
-        <span className='text-sm font-normal leading-normal'> Details </span>
+        <span className='text-sm text-center font-normal leading-normal'> Details </span>
+      </div>
+      <div class='rounded-invert-left'>
+        <span className='heading-6'>Special deal</span>
+        <span className='text-sm font-normal leading-5 text-gray-500'> For orders over $0.01 excluding freight </span>
+        <span className='text-xs font-normal text-gray-500'>Valid until 2024/3/20</span>
       </div>
     </div>
   )
--- src/pages/UserCouponManagement/UserCouponManagement.tsx
+++ src/pages/UserCouponManagement/UserCouponManagement.tsx
@@ -4,14 +4,19 @@ import OrderList from "../../components/OrderList";
 import Coupon from "../../components/Coupon/Coupon";
 
 export function UserCouponManagement() {
+  const index = [1, 2, 3, 4, 5, 6]
   return (
     <Container>
       <Tabs aria-label="Tabs with underline" style="underline">
         <Tabs.Item active title="Unused coupons">
-          <Coupon />
+          <div className="grid grid-cols-3 justify-items-center w-full gap-[2rem] ">
+            {index.map((index) => (
+              <Coupon key={index} />
+            ))}
+          </div>
           <Pagination className="ml-auto mr-auto text-center" currentPage={0} onPageChange={function (page: number): void {
             throw new Error("Function not implemented.");
-          } } totalPages={3} />
+          }} totalPages={3} />
         </Tabs.Item>
         <Tabs.Item title="Used coupons">Disabled content</Tabs.Item>
         <Tabs.Item title="Expired coupons">Disabled content</Tabs.Item>
